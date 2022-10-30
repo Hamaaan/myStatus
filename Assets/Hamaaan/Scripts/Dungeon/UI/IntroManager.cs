0c99cb46a27f1c8cf28cd242b341c692ee310431
Hamaaan <51164087+Hamaaan@users.noreply.github.com>
2022-10-30 23:29:45 +0900

    d

---
+++ Assets/Hamaaan/Scripts/Dungeon/UI/IntroManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+
+public class IntroManager : MonoBehaviour
+{
+    [SerializeField] string SceneName;
+    VideoPlayer vp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vp = GetComponent<VideoPlayer>();
+        vp.loopPointReached += LoopPointReached;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoopPointReached(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
