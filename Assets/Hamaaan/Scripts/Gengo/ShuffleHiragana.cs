fa53b3654f6a7b056c18ff15f7972bc088bbbc66
wonokokko <115980348+wonokokko@users.noreply.github.com>
2022-11-24 08:45:16 +0900

    opUI

---
+++ Assets/Hamaaan/Scripts/Gengo/ShuffleHiragana.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShuffleHiragana : MonoBehaviour
+{
+    public List<GameObject> HiraganaList;
+    public List<GameObject> HiraganaUseList = new List<GameObject>();
+    private GameObject randomObj;
+    private int choiceNum;
+ 
+    void Start()
+    {
+         for(int i=0; i<8; i++)
+        {
+            //HiraganaListの中からランダムで1つを選ぶ
+            //randomObj = HiraganaList[Random.Range(0, HiraganaList.Count)];
+            randomObj = HiraganaList[Random.Range(0, 1)];
+            //選んだオブジェクトをuseListに追加
+            HiraganaUseList.Add(randomObj);
+            //選んだオブジェクトのリスト番号を取得
+            choiceNum = HiraganaList.IndexOf(randomObj);
+            //同じリスト番号をHiraganaListから削除
+            HiraganaList.RemoveAt(choiceNum);
+        }
+    }
+}
