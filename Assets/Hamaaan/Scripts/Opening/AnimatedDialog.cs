b77ead296114baf2ae61e38786f40cd3dae759b0
SotSpepo <yuuki.072092@gmail.com>
2022-11-23 17:23:46 +0900

    Opening作成

---
+++ Assets/Hamaaan/Scripts/Opening/AnimatedDialog.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimatedDialog : MonoBehaviour
+{
+    //アニメーター
+    [SerializeField] private Animator animator;
+
+    //レイヤー
+    [SerializeField] private int layer;
+
+    //フラグ
+    private static readonly int ParamIsOpen = Animator.StringToHash("IsOpen");
+
+    //ダイアログは開いてるかどうか
+    public bool IsOpen => gameObject.activeSelf;
+
+    //アニメーション中かどうか
+    public bool IsTransition { get; private set; }
+
+    public void Open()
+    {
+        //不正防止
+        if (IsOpen || IsTransition) return;
+
+        //アクティブにする
+        gameObject.SetActive(true);
+
+        animator.SetBool(ParamIsOpen, true);
+
+        StartCoroutine(WaitAnimation("Opened"));
+    }
+    public void Close()
+    {
+        if (!IsOpen || IsTransition) return;
+
+        animator.SetBool(ParamIsOpen, false);
+
+        StartCoroutine(WaitAnimation("Closed", () => gameObject.SetActive(false)));
+    }
+
+    private IEnumerator WaitAnimation(string stateName, UnityAction onCompleted = null)
+    {
+        IsTransition = true;
+
+        yield return new WaitUntil(() =>
+        {
+            var state = animator.GetCurrentAnimatorStateInfo(layer);
+            return state.IsName(stateName) && state.normalizedTime >= 1;
+        }
+        );
+
+        IsTransition = false;
+
+        onCompleted?.Invoke();
+    }
+}
---
+++ Assets/Hamaaan/Scripts/Opening/OpeningSceneManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OpeningSceneManager : MonoBehaviour
+{
+    [SerializeField] public Text InputName;
+    [SerializeField] public Text DisplayName;
+
+    [SerializeField] string SceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InputName = InputName.GetComponent<Text>();
+        DisplayName = DisplayName.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DisplayName.text = InputName.text;
+    }
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
