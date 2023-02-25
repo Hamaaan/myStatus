9b5774d61ecc7ce11ace870f2ba7039606653a25
Hamaaan <51164087+Hamaaan@users.noreply.github.com>
2023-02-25 20:20:41 +0900

    hamm

---
+++ Assets/Hamaaan/Scripts/Dungeon/AutoRestart.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class AutoRestart : MonoBehaviour
+{
+    [SerializeField] GameObject StaticCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((int)(Time.realtimeSinceStartup%120) == 0) 
+        {
+            SceneManager.LoadScene("Opening 1");
+        }
+
+        Debug.Log(Time.realtimeSinceStartup);
+
+        if (StaticValueManager.instance.ActiveSceneName == "Opening 1")
+        {
+            StaticCanvas.SetActive(false);
+        }
+        else
+        {
+            StaticCanvas.SetActive(true);
+        }
+    }
+}
